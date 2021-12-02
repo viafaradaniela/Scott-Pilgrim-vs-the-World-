baaf5cb4eaa211b6ea18d4360f09d486d676cc8b
Maria Jose Barney <maria.barney1@correo.icesi.edu.co>
2021-12-02 15:36:29 -0500

    Plantilla

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(1200, 700);
+}
+
+function draw() {
+
+}
+
+function mousePressed() {
+  
+}
+
+function mouseReleased() {
+
+}
+
+function keyPressed() {
+
+}
+
+function keyReleased() {
+
+}
