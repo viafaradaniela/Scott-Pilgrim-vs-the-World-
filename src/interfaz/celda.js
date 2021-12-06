ee01bdac02d773d4e9e0746929b375e8e2623a5b
Maria Jose Barney <maria.barney1@correo.icesi.edu.co>
2021-12-06 16:40:14 -0500

    Logica del celdas

---
+++ src/interfaz/celda.js
@@ -0,0 +1,45 @@
+class Celda {
+    constructor(escenario, pos, location, index, type) {
+        this.escenario = escenario;
+        this.app = escenario.app;
+        this.location = location;
+        this.pos = pos;
+        this.width = 100;
+        this.height = 100;
+        this.index = index;
+        this.type = type;
+        this.views = escenario.views;
+    }
+
+    draw() {
+        const view = this.getView();
+        if (view !== undefined) {
+            this.app.imageMode(this.app.CENTER)
+            this.app.image(view, this.pos.x, this.pos.y)
+        }
+
+    }
+
+
+    getView() {
+        return this.views[this.type];
+    }
+
+    getPos() {
+        return {
+            x: this.pos.x,
+            y: this.pos.y
+        }
+    }
+}
+
+
+const CELDA = {
+    VACIO1: 0,
+    VACIO2: 1,
+    ARMA: 2,
+    MONEDA: 3,
+    VIDA: 4,
+    MURO1: 5,
+    MURO2: 6
+}
